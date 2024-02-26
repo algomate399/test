d404857f60d54c120ba153d536113f2d1903c645
algomate399 <155889177+algomate399@users.noreply.github.com>
2024-02-26 14:03:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,5 +10,5 @@ def index():
     expiry_dates = nse.get_expiry()
     return render_template('index.html', expiry_dates=expiry_dates)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
