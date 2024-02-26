9f813c4df11afe3023c00229767cd54076a78db2
algomate399 <155889177+algomate399@users.noreply.github.com>
2024-02-26 13:09:06 +0530

    Add files via upload

--- database.py
+++ database.py
@@ -2,21 +2,33 @@
 import requests
 from datetime import datetime
 
-def get_expiry(indices):
-    input_format = "%d-%b-%Y"
-    output_format = "%d%b%y"
-    url = f'https://www.nseindia.com/api/option-chain-indices?symbol={indices}'
-    headers = {
-        "Accept": "*/*",
-        "Accept-Encoding": "gzip, deflate, br",
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36",
-        "Connection": "keep-alive"
-    }
-    response = requests.get(url, headers=headers)
-    print('resp',response)
-    if response.status_code == 200:
-        records = response.json()['records']
-        format_exp = [datetime.strptime(date, input_format).strftime(output_format).upper() for date in records['expiryDates']]
-        return format_exp
-    else:
-        return []  # or handle the error in an appropriate way
+
+class NSE_Session:
+    def __init__(self, symbol,timeout=5):
+        self.__url = "https://www.nseindia.com/api/option-chain-indices?symbol={}".format(symbol)
+        self.__session = requests.sessions.Session()
+        self.__session.headers = {
+            "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:102.0) Gecko/20100101 Firefox/102.0",
+            "Accept": "*/*", "Accept-Language": "en-US,en;q=0.5"}
+        self.__timeout = timeout
+        self.__session.get("https://www.nseindia.com/option-chain", timeout=self.__timeout)
+
+    def get_expiry(self):
+        input_format = "%d-%b-%Y"
+        output_format = "%d%b%y"
+        try:
+            response = self.__session.get(url=self.__url, timeout=self.__timeout)
+            if response.status_code == 200:
+                records = response.json()['records']
+                format_exp = [datetime.strptime(date, input_format).strftime(output_format).upper() for date in
+                              records['expiryDates']]
+                return format_exp
+            else:
+                return []
+
+        except Exception as ex:
+            print('Error: {}'.format(ex))
+            self.__session.get("https://www.nseindia.com/option-chain", timeout=self.__timeout)
+
+
+
--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from flask import Flask, render_template
-from database import get_expiry  # Replace 'your_module' with the module where get_expiry function is defined
+from database import NSE_Session  # Replace 'your_module' with the module where get_expiry function is defined
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     indices = "NIFTY"  # Replace with the indices you want to fetch expiry dates for
-    expiry_dates = get_expiry(indices)
+    nse = NSE_Session(indices)
+    expiry_dates = nse.get_expiry()
     return render_template('index.html', expiry_dates=expiry_dates)
 
-# if __name__ == '__main__':
-#     app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
